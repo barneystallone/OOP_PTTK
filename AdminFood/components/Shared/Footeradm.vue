a6bc59826c849e02447b0c20b55dfff683da4f62
Vinh Nguyen <thvinh2001@gmail.com>
2023-01-04 19:25:15 +0700

    Upload full code demo

---
+++ AdminFood/components/Shared/Footeradm.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+  <div class="text-center p-2" style="background: linear-gradient(to right, #0bc539, #eef110) !important">
+    © 2021 CNTTD
+    <a class="text-reset fw-bold" href="https://mdbootstrap.com/"></a>
+  </div>
+  </footer>
+</template>
+<script>
+export default {}
+</script>
